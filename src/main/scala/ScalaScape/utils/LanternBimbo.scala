a041048786e19974e9b676fe67311ac338e3986d
William Raendchen <william.raendchen@easybell.de>
2024-10-16 00:00:00 +0200

    cleanup
    GitPrivacy: ApAobJnd730V1EE+vzzOAqCQ/AAM+AhcKXpuD/9byM+lR6p+uB+G/sbK2kZK6zz4lEcrrtXrrbg= PoPv+YeZtPnBh90jvPkfQiu/1vEnRCK9pS/laVUQVLk3tgbhYkNIoyuYRIRYb+bIRt4RLvy2/rc=

---
+++ src/main/scala/ScalaScape/utils/LanternBimbo.scala
@@ -0,0 +1,34 @@
+package ScalaScape.utils
+
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.terminal.swing.SwingTerminalFontConfiguration
+import com.googlecode.lanterna.terminal.{DefaultTerminalFactory, Terminal}
+
+import java.awt.{Font, GraphicsEnvironment}
+import javax.swing.JFrame
+import javax.swing.WindowConstants.EXIT_ON_CLOSE
+
+class LanternBimbo {
+  def makeTerminal(forceTerminal: Boolean): Terminal = {
+    def getFont(family: String, style: Int, size: Int): Font = {
+      val availableFonts = GraphicsEnvironment.getLocalGraphicsEnvironment.getAvailableFontFamilyNames
+      if availableFonts.contains(family) then new Font(family, style, size)
+      else new Font("Monospaced", style, size)
+    }
+
+    val terminalFactory = new DefaultTerminalFactory()
+    val fontConfig = SwingTerminalFontConfiguration.newInstance(getFont("Consolas", Font.PLAIN, 20))
+    terminalFactory.setInitialTerminalSize(new TerminalSize(120, 30))
+    terminalFactory.setTerminalEmulatorFontConfiguration(fontConfig)
+    terminalFactory.setPreferTerminalEmulator(!forceTerminal)
+    terminalFactory.setForceTextTerminal(forceTerminal)
+
+    terminalFactory.createTerminal() match {
+      case frame: JFrame =>
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE)
+        frame
+      case terminal: Terminal =>
+        terminal
+    }
+  }
+}
