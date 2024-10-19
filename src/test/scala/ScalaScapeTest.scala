d2c2f036be922529742b40d7a2d48e9f50a96797
William Raendchen <william.raendchen@gmail.com>
2024-10-20 00:00:00 +0200

    fix formatting
    GitPrivacy: 6LEQ8c1yCgbKB3z7iSpX5uOJ3yTeokQlzWfGPOaA9DNqzb/EffvOuzoPeQ0lIW5MTUUR9Lx73rc= KptbfRw0Ly5cFSFM5lZYm8YgaX4IhoZUTwKqZ16pw/qf9DbQlo7i4X1DMyiVHgVMvS9H3vS6qJk=

--- src/test/scala/ScalaScapeTest.scala
+++ src/test/scala/ScalaScapeTest.scala
@@ -1,13 +1,12 @@
 import org.scalatest.funsuite.AnyFunSuite
 import ScalaScape.ScalaScape
-import _root_.ScalaScape.components.GameState
 
 class ScalaScapeTest extends AnyFunSuite {
-    test("the game can run 100 ticks without crashing") {
-      val game = new ScalaScape(forceTerminal = false)
-      
-      for (_ <- 1 to 100) {
-        var state = game.update(game.state)
-      }
+  test("the game can run 100 ticks without crashing") {
+    val game = new ScalaScape(forceTerminal = false)
+
+    for (_ <- 1 to 100) {
+      var state = game.update(game.state)
     }
+  }
 }
