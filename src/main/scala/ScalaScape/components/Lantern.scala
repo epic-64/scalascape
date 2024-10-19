63cf79de9c7ae0d6217663fa391565b5f7614d57
William Raendchen <william.raendchen@gmail.com>
2024-10-20 00:00:00 +0200

    cleanup
    GitPrivacy: Up15eXVJ8w5TocOjk0ZRe10ed/Vu1VdaP29NiIg9vA2I2BVh9uNADBVn13/LyMy1iy25VheXc5k= hrKYaLAI60FrwaQ+Ug8fK6Hgwg63LWPM8NLrwrHEqegcZRXdOIaYX5h5UJVLeKc/Md0uC9pxSYs=

--- src/main/scala/ScalaScape/components/Lantern.scala
+++ src/main/scala/ScalaScape/components/Lantern.scala
@@ -7,6 +7,24 @@ import com.googlecode.lanterna.graphics.TextGraphics
 case class Pos(x: Int, y: Int)
 case class TerminalString(content: String, position: Pos, color: TextColor = DEFAULT)
 
+
+case class LineWord(content: String, color: TextColor = TextColor.ANSI.DEFAULT)
+
+case class TerminalLine(words: List[LineWord], pos: Pos):
+  def render(): TerminalParagraph =
+    val x = pos.x
+    
+    // create a list of TerminalString objects, each one offset by the length of the previous string
+    val terminalStrings = words.zipWithIndex.map { case (word, index) =>
+      val offset = words.take(index).map(_.content.length).sum
+      val newPos = Pos(x + offset, pos.y)
+      TerminalString(word.content, newPos, word.color)
+    }
+    
+    TerminalParagraph(terminalStrings)
+  end render
+end TerminalLine
+
 case class TerminalParagraph(list: List[TerminalString]):
   def ++(other: TerminalParagraph | List[TerminalString] | TerminalString): TerminalParagraph =
     other match
@@ -30,25 +48,21 @@ case class ProgressBarParameters(
     progress: Between0And1,
     position: Pos,
     color: TextColor,
-    leftLimiter: String = "[",
-    rightLimiter: String = "]"
+    filledChar: Char = '#',
+    emptyChar: Char = ':'
 )
 
 object ProgressBar:
-  def from(p: ProgressBarParameters): List[TerminalString] =
-    val x             = p.position.x
-    val y             = p.position.y
-    val innerLength   = p.width - 2 // Reserve space for boundaries
-    val filledLength  = (p.progress * innerLength).toInt
-    val fillChar      = ':'
-    val filledSection = (1 to filledLength).map(_ => fillChar).mkString
-    val emptySection  = (1 to (p.width - innerLength)).map(_ => " ").mkString
+  def from(par: ProgressBarParameters): List[TerminalString] =
+    val x             = par.position.x
+    val y             = par.position.y
+    val filledLength  = (par.progress * par.width).toInt
+    val filledSection = (1 to filledLength).map(_ => par.filledChar).mkString
+    val emptySection  = ((1 + filledLength) to par.width).map(_ => ":").mkString
 
     List(
-      TerminalString(p.leftLimiter, Pos(x, y), WHITE),
-      TerminalString(filledSection, Pos(x + 1, y), p.color),
-      TerminalString(emptySection, Pos(x + 1 + filledLength, y), TextColor.ANSI.DEFAULT),
-      TerminalString(p.rightLimiter, Pos(x + p.width - 1, y), WHITE)
+      TerminalString(filledSection, par.position, par.color),
+      TerminalString(emptySection, Pos(x + filledLength, y), TextColor.ANSI.BLACK_BRIGHT)
     )
   end from
 end ProgressBar
--- src/main/scala/ScalaScape/components/Skills.scala
+++ src/main/scala/ScalaScape/components/Skills.scala
@@ -29,7 +29,7 @@ trait Skill:
 
     state
   end update
-  
+
   private def gainXp(amount: Int): Unit = {
     xp += amount
 
@@ -45,54 +45,70 @@ end Skill
 trait SubSkill extends Skill:
   def parent(state: GameState): Skill
 
-  override def xpForNextLevel: Int         = level * 50
-  
+  override def xpForNextLevel: Int = level * 50
+
   override def update(state: GameState): GameState =
     parent(state).update(state) // update parent skill first
-    super.update(state)         // then update sub skill (using the same logic
+    super.update(state) // then update sub skill (using the same logic
   end update
 
   def render(pos: Pos, state: GameState): TerminalParagraph =
-    val x = pos.x
-    val y = pos.y
-    val pb = ProgressBarParameters
-    
-    val par: Skill = parent(state)
-    val parentXpString = s"${par.name} (${par.level} / 99) -> ${par.xp} / ${par.xpForNextLevel}"
-    def skillXpBar = TerminalParagraph(
-      List(TerminalString(parentXpString, Pos(x, y), WHITE))
-        ++ ProgressBar.from(pb(40, par.progressToNextLevel, Pos(x, y + 1), BLUE_BRIGHT))
-    )
-    
-    def masteryXpBar = TerminalParagraph(
-      List(TerminalString(s"$name ($level / 99) -> $xp / $xpForNextLevel", Pos(x, y + 3), WHITE))
-        ++ ProgressBar.from(pb(40, progressToNextLevel, Pos(x, y + 4), CYAN))
-    )
+    val x     = pos.x
+    val y     = pos.y
+    val pb    = ProgressBarParameters
+    val width = 40
 
-    def actionBar = TerminalParagraph(
-      List(
-        TerminalString(s"Action Progress: ETA: ", Pos(x, y + 6), WHITE),
-        TerminalString(f"$remainingDuration%1.1f", Pos(x + 22, y + 6), CYAN_BRIGHT),
-        TerminalString(" seconds", Pos(x + 26, y + 6), WHITE)
+    def skillXpBar: TerminalParagraph = {
+      val par: Skill      = parent(state)
+      val parentXpString  = s"${par.name} (${par.level} / 99)"
+      val parentXpString2 = s"${par.xp} / ${par.xpForNextLevel}"
+
+      TerminalParagraph(
+        List(
+          TerminalString(parentXpString, Pos(x, y), WHITE),
+          TerminalString(parentXpString2, Pos(x + width - parentXpString2.length, y), BLACK_BRIGHT)
+        )
       )
-        ++ ProgressBar.from(pb(40, actionProgress, Pos(x, y + 7), GREEN_BRIGHT))
-    )
+        ++ ProgressBar.from(pb(width, par.progressToNextLevel, Pos(x, y + 1), BLUE_BRIGHT))
+    }
 
-    TerminalParagraph(skillXpBar.list ++ masteryXpBar.list ++ actionBar.list)
+    def masteryXpBar(offset: Int) = {
+      val str1 = s"$name ($level / 99)"
+      val str2 = s"$xp / ${xpForNextLevel}"
+
+      TerminalParagraph(
+        List(
+          TerminalString(str1, Pos(x, y + offset), WHITE),
+          TerminalString(str2, Pos(x + width - str2.length, y + offset), BLACK_BRIGHT)
+        )
+      ) ++ ProgressBar.from(pb(width, progressToNextLevel, Pos(x, y + offset + 1), CYAN))
+    }
+
+    def actionBar(offset: Int) =
+      TerminalLine(
+        List(
+          LineWord("Action Progress: ETA: ", WHITE),
+          LineWord(f"$remainingDuration%1.1f", CYAN_BRIGHT),
+          LineWord(" seconds", WHITE)
+        ),
+        Pos(x, y + offset)
+      ).render()
+        ++ ProgressBar.from(pb(width, actionProgress, Pos(x, y + offset + 1), GREEN_BRIGHT))
+
+    TerminalParagraph(skillXpBar.list ++ masteryXpBar(3).list ++ actionBar(6).list)
   end render
 end SubSkill
 
 case class Woodcutting() extends Skill:
-  override val name: String = "Woodcutting Skill"
-  
-  override def onComplete(state: GameState, gainedXp: WidthInColumns): Unit = {
+  override val name: String = "Woodcutting"
+
+  override def onComplete(state: GameState, gainedXp: WidthInColumns): Unit =
     state.activityLog.add(s"Got $gainedXp XP in Woodcutting.")
-  }
 end Woodcutting
 
 class WoodCuttingOak() extends SubSkill:
   override val name: String = "Oak Mastery"
-  
+
   override def parent(state: GameState): Woodcutting = state.skills.woodcutting
 
   def getInventorySlot(state: GameState): InventoryItem = state.inventory.items("Oak")
