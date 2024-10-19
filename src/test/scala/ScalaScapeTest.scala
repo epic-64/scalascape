764bc107a86eb83835080c2d10fa3f9752dc032c
William Raendchen <william.raendchen@gmail.com>
2024-10-20 00:00:00 +0200

    add test for running the game
    GitPrivacy: MFcmptJKZKfH5JxfDY06s6kmnI+hFXt/x4GJYGaKQYVCxERDsre6i//7mgJKs/RQz34PbWUZlqo= mMOlJcXJbt4GFTOV8T3HSUx4Oth2SF4ZUj/FyRqg4N9jUnWcy9PgqWdMrmlGWQQBKK8/RPOpbMQ=

--- src/test/scala/ScalaScapeTest.scala
+++ src/test/scala/ScalaScapeTest.scala
@@ -1,5 +1,13 @@
 import org.scalatest.funsuite.AnyFunSuite
+import ScalaScape.ScalaScape
+import _root_.ScalaScape.components.GameState
 
 class ScalaScapeTest extends AnyFunSuite {
-
+    test("the game can run 100 ticks without crashing") {
+      val game = new ScalaScape(forceTerminal = true)
+      
+      for (_ <- 1 to 100) {
+        var state = game.update(game.state)
+      }
+    }
 }
