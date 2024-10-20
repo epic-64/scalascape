43a2b1ef4488060ee72718f7fb5adb0424b34af9
William Raendchen <william.raendchen@gmail.com>
2024-10-21 00:00:00 +0200

    add test
    GitPrivacy: Ur5r64LV1Y5tm07BOkFXgbrpbb5fYrpYcG6cUBVJlxd7tpqYnb41wxIyk8MmcVH0rrStb6lE0Nc= iOSwTyfNypuyFp9jd3vpH5QchHpf2rm9Y1ZS+bbYML35og6IbnxSYwiGFayxdRcihMIww8ZXOxM=

--- src/test/scala/ScalaScapeTest.scala
+++ src/test/scala/ScalaScapeTest.scala
@@ -1,6 +1,6 @@
 import org.scalatest.funsuite.AnyFunSuite
 import ScalaScape.ScalaScape
-import ScalaScape.components.WoodCuttingOakScene
+import ScalaScape.components.*
 import com.googlecode.lanterna.graphics.TextGraphics
 import com.googlecode.lanterna.screen.TerminalScreen
 import org.scalatestplus.mockito.MockitoSugar
@@ -10,12 +10,29 @@ class ScalaScapeTest extends AnyFunSuite with MockitoSugar {
     val mockScreen   = mock[TerminalScreen]
     val mockGraphics = mock[TextGraphics]
     val game         = new ScalaScape(mockScreen, mockGraphics)
-    
+
     game.state.swapScene(WoodCuttingOakScene())
-    
+
     for (_ <- 1 to 100) {
       game.update(game.state)
       game.draw(mockGraphics) // rendering is tested here, drawing is not
     }
+  }
+  
+  test("the skill values are persisted throughout scene swaps") {
+    val mockScreen   = mock[TerminalScreen]
+    val mockGraphics = mock[TextGraphics]
+    val game         = new ScalaScape(mockScreen, mockGraphics)
+
+    game.state.skills.woodcutting.mastery[WoodCuttingOak].xp = 100
+    game.state.skills.woodcutting.mastery[WoodCuttingOak].level = 5
+
+    game.state.swapScene(WoodCuttingTeakScene())
+    game.update(game.state)
+    
+    game.state.swapScene(WoodCuttingOakScene())
+
+    assert(game.state.skills.woodcutting.mastery[WoodCuttingOak].xp == 100)
+    assert(game.state.skills.woodcutting.mastery[WoodCuttingOak].level == 5)
   }
 }
