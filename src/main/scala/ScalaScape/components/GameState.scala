08d25310e5b53df84c7e5b34eef648a5f3406c2d
William Raendchen <william.raendchen@easybell.de>
2024-10-21 13:36:04 +0200

    inject state into all scenes

--- src/main/scala/ScalaScape/components/GameState.scala
+++ src/main/scala/ScalaScape/components/GameState.scala
@@ -2,7 +2,7 @@ package ScalaScape.components
 
 class GameState:
   val targetFps: Int       = 30
-  var selectedScene: Scene = WorldMenuScene()
+  var selectedScene: Scene = WorldMenuScene(this)
   var activityLog          = ActivityLog()
   var inventory            = Inventory()
 
