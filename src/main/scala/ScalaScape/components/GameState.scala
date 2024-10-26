5fb0ea6cf6a40a8d7799430b59bfbda11f055b2c
William Raendchen <william.raendchen@gmail.com>
2024-10-26 20:16:57 +0200

    cleanup

--- src/main/scala/ScalaScape/components/GameState.scala
+++ src/main/scala/ScalaScape/components/GameState.scala
@@ -1,16 +1,16 @@
 package ScalaScape.components
 
 class GameState(val targetFps: Int):
-  var activityLog          = ActivityLog()
-  var inventory            = Inventory()
+  val activityLog = ActivityLog()
+  val inventory   = Inventory()
+  val skills      = SkillList()
+  val scenes      = SceneList(this)
 
-  var skills: SkillList = new SkillList()
-  val scenes: SceneList = new SceneList(this)
-  var selectedScene: Scene = scenes.world
+  private var selectedScene: Scene = scenes.world
+  var forceClearScreen: Boolean    = false
 
-  var forceClearScreen: Boolean = false
-
-  def getScene: Scene = selectedScene
+  def update(): GameState                = selectedScene.update(this)
+  def getScene: Scene                    = selectedScene
   def swapScene(scene: Scene): GameState =
     selectedScene = scene
     activityLog.add(s"Entered ${scene.name}")(this)
@@ -18,17 +18,15 @@ class GameState(val targetFps: Int):
     this
   end swapScene
 
-  def update(): GameState = selectedScene.update(this)
-
   class SkillList {
     val woodcutting: Woodcutting = Woodcutting()
   }
 
   class SceneList(state: GameState):
-    val world: WorldMenuScene = WorldMenuScene(state)
-    val gathering: GatheringMenuScene = GatheringMenuScene(state)
+    val world: WorldMenuScene             = WorldMenuScene(state)
+    val gathering: GatheringMenuScene     = GatheringMenuScene(state)
     val woodcutting: WoodCuttingMenuScene = WoodCuttingMenuScene(state)
-    val oak: OakScene = OakScene(state)
-    val teak: TeakScene = TeakScene(state)
+    val oak: OakScene                     = OakScene(state)
+    val teak: TeakScene                   = TeakScene(state)
   end SceneList
 end GameState
